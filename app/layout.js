b48fcd6f4d5f6a814ea9b69194b669b34269deee
Mateo Algañarás <tutealga@gmail.com>
2024-04-04 01:01:00 -0300

    favicon

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 
 export const metadata = {
-  title: 'Monad ecosystem',
+  title: 'Monad | Ecosystem',
   description: 'Monad - Extreme Parallelized Performance for EVM',
 }
 
