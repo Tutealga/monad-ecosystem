1bf20e149e51ee337ee10de5b6c7429577b30c90
Mateo Algañarás <tutealga@gmail.com>
2024-04-04 00:43:56 -0300

    Resize project img

--- components/Product.js
+++ components/Product.js
@@ -3,7 +3,7 @@ const Product = ({product}) => {
   return (
 <a href={product.website} target="__blank" className="flex flex-col text-white items-center justify-center gap-2 hover:scale-110">
         <div style={{backgroundImage: `url('https://assets-global.website-files.com/64c26cc84790d118b80c38c9/6529c5fdb05ba5609da216c9_monad-logo-mark-full-color-rgb.svg')`}} className="w-[75px] h-[75px] sm:w-[125px] monad sm:h-[125px] bg-center bg-no-repeat bg-cover flex items-center justify-center">
-            <img src={product.img} className="sm:w-[75px] sm:h-[75px] h-[40px] w-[40px] rounded-full"/>
+            <img src={product.img} className="sm:w-[75px] sm:h-[75px] h-[45px] w-[45px] rounded-full"/>
             </div>
             <h3 className="text-base w-full text-center">{product.name}</h3>
             </a>       
