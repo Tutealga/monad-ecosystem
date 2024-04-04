3318d02580a065778d2950010cf9fb46fc6ae9bd
Mateo Algañarás <tutealga@gmail.com>
2024-04-04 00:49:28 -0300

    Resize background img

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="relative bg-[rgb(32,0,82)] text-[#F7EDE8] p-16">
       <div className="mobius-floating-div z-10">
-        <img src="https://assets-global.website-files.com/647f71a77a2f4691b4fa23a7/647f71a77a2f4691b4fa2422_monad-purple-30-percent%403x-8.png" loading="lazy" data-w-id="2449f98d-1c61-889a-da0a-2ff1fb523866"/>
+        <img className="w-[200%] sm:w-full" src="https://assets-global.website-files.com/647f71a77a2f4691b4fa23a7/647f71a77a2f4691b4fa2422_monad-purple-30-percent%403x-8.png" loading="lazy" data-w-id="2449f98d-1c61-889a-da0a-2ff1fb523866"/>
         </div>
         {children}
         </body>
