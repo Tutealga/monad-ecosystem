0fb04bf8445bfded6c0e58cb8e15fc110bf9b7e2
Mateo Algañarás <tutealga@gmail.com>
2024-04-05 01:18:09 -0300

    Mobile responsive changes

--- components/Projects.js
+++ components/Projects.js
@@ -2,10 +2,12 @@ const Projects = ({project}) => {
 
   return (
    <a href={project.website ? project.website : project.x} className="hover:scale-110 border-[#836EF9] border bg-[#836EF9] bg-opacity-30 rounded-[5px]">
-     <div className="flex flex-row p-6 items-center justify-between">
-       <div className="flex flex-col gap-2">
-         <h3 className="text-base font-bold">{project.name}</h3>
+     <div className="flex flex-row p-6 justify-between">
+       <div className="flex flex-col gap-2 justify-between">
+        <div>
+        <h3 className="text-base font-bold">{project.name}</h3>
          <span className="text-sm">{project.subcategory}</span>
+        </div>
          <div className="flex flex-row gap-2">
            <a className="w-[1rem] h-auto" href={project.x}>
              <img src="https://assets-global.website-files.com/647f71a77a2f4691b4fa23a7/647f71a77a2f4691b4fa23e0_tw.png"/>
