e0f7ed9861e894cfa4c53b2fd6602778a1e807a1
Mateo Algañarás <tutealga@gmail.com>
2024-06-28 20:22:43 -0300

    Style changes

--- app/layout.js
+++ app/layout.js
@@ -4,17 +4,17 @@ import Mobius from '@/components/Mobius'
 import '@/styles/globals.css'
 
 export const metadata = {
-  title: 'Monad | Ecosystem',
-  description: 'Monad - Extreme Parallelized Performance for EVM',
+  title: 'Monad Ecosystem',
+  description: 'Monad ecosystem is an informative page where you can find the different applications of the Monad ecosystem',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="relative bg-[rgb(32,0,82)] text-[#F7EDE8] px-16">
+      <body className="relative bg-[rgb(32,0,82)] text-[#F7EDE8] md:px-16 px-4">
         <Mobius/>
         <Header/>
-        <main className="flex mx-auto min-h-screen flex-col w-full max-w-[1200px]">
+        <main className="flex mx-auto min-h-screen flex-col w-full max-w-full md:max-w-[1200px]">
         {children}
         </main>
         <Footer/>
